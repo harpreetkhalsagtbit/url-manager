ce22475035998e9ff3ec29c2b1da18953e39a2ab
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2017-06-19 18:42:06 +0530

    Add: Semantic React Ui - Implemented and tested

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import * as authActions from './actions/AuthAction';
 import {connect} from 'react-redux';
 
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Button } from 'semantic-ui-react'
 
 class App extends Component {
 
@@ -25,6 +27,9 @@ class App extends Component {
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
                 <main>
+                    <Button>
+                        Click Here
+                    </Button>
                 </main>
 
             </div>
