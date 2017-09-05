634f40958c56534baadacb443c9d74a973e3e164
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2017-09-05 16:24:24 +0530

    Add: modal

--- src/components/common/Test.js
+++ src/components/common/Test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // ES6
 import {TextArea } from 'semantic-ui-react'
+import ContextMenu from './ContextMenu/ContextMenu'
+import './ContextMenu/ContextMenu.css';
 
-const Test = ({name, label, onChange, rows, placeHolder, value="", error}) => {
+const Test = ({name}) => {
 
 	return (
 		<div className="">
+			<ul className="tasks">
+				<li className="task" data-id="3">
+					<div className="task__content">
+						Go To Grocery
+					</div>
+					<div className="task__actions">
+						<i className="fa fa-eye"></i>
+						<i className="fa fa-edit"></i>
+						<i className="fa fa-times"></i>
+						</div>
+				</li>
+	    	</ul>
 		</div>
 
 	);
